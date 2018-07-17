636e34c0a3822388f52fe0e5016d2ce29e30eb68
gzyushizhan <gzyushizhan@corp.netease.com>
2018-07-17 10:50:36 +0800

    Impl register flow

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,10 @@
+#encoding=utf8
+
 from flask_wtf import FlaskForm
 from wtforms import *
 from wtforms.validators import *
+from wtforms import ValidationError
+from ..models import *
 
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
@@ -9,7 +13,16 @@ class RegisterForm(FlaskForm):
     password2 = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password', message='password must be eual')])
     submit = SubmitField('Register')
 
+    # Handle all validate in Forms    
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9]*$', 0, 'Only support letters, numbers')])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    submit = SubmitField('Login')
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -8,6 +9,17 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author')
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attributed')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     __tablename__ = 'posts'
