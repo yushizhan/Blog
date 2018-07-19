ca2e59a5f8b012749ab9ec59b0fd8cd1e16212b4
gzyushizhan <gzyushizhan@corp.netease.com>
2018-07-19 11:05:22 +0800

    Add Post lgoic

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,20 @@
-from flask import render_template, session
-from ..models import User
+from flask import render_template, session, redirect, url_for
+from ..models import User, Post
 from . import main
+from .forms import PostForm
+from .. import db
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    # Login state
+    form = PostForm()
+    user = None
     if 'username' in session:
         user = User.query.filter_by(username=session['username']).first()
-        return render_template('index.html', current_user=user)
-    else:
-        return render_template('index.html', current_user=None)
+    if user and form.validate_on_submit():
+        post = Post(body=form.body.data, author=user)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', current_user=user, form=form, posts=posts)
+
